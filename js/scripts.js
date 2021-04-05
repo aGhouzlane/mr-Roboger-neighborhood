78a51c663231a6052a09d7abeede5f3dd13c30a2
aGhouzlane <ahmedghouzlane@gmail.com>
2021-04-05 10:20:52 -0400

    Add some stylings to the project

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,10 @@ $(document).ready(function () {
     });
   });
 
+  $(function () {
+    showText("#msg", "Welcome to Mr. Roboger Neighborhood!", 0, 70);
+  });
+
 });
 
 //Business logic
@@ -22,43 +26,40 @@ function beepBoopBemyfriend(userInput) {
   var stringArray = inputArray.map(String);
   var outputArray = [];
 
-  //console.log(stringArray);
-
   for (let i = 0; i < stringArray.length; i++) {
     if (stringArray[i].includes('1') && stringArray[i].includes('2') && stringArray[i].includes('3')) {
-      //console.log("won't you be my neighbor!");
       outputArray[i] = "won't you be my neighbor!";
     }
     else if (stringArray[i].includes('1') && stringArray[i].includes('2')) {
-      //console.log("boop!");
       outputArray[i] = "boop!";
     }
     else if ((stringArray[i].includes('1') || stringArray[i].includes('2')) && stringArray[i].includes('3')) {
-      //console.log("won't you be my neighbor!");
       outputArray[i] = "won't you be my neighbor!";
     }
     //case includes all the 1s
     else if (stringArray[i].includes('1')) {
-      //console.log("beep!");
       outputArray[i] = "beep!";
     }
     //case includes all the 2s
     else if (stringArray[i].includes('2') && !stringArray[i].includes('3')) {
-      //console.log("boop!");
       outputArray[i] = "boop!";
     }
-
     // Case includes all the 3s
     else if (stringArray[i].includes('3')) {
-      //console.log("won't you be my neighbor!");
       outputArray[i] = "won't you be my neighbor!";
     }
     else {
-      //console.log(stringArray[i]);
       outputArray[i] = stringArray[i];
     }
   }
   return outputArray;
+}
+
+var showText = function (target, message, index, interval) {
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
 }
 
 
